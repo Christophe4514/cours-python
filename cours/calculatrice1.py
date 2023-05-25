0bfc1e5e7eed4bfd768d0c84371543a2b1b8fb92
christophe4514 <christophewizamo@gmail.com>
2023-05-25 13:33:44 +0100

    exercices boucles suite

--- cours/calculatrice1.py
+++ cours/calculatrice1.py
@@ -5,5 +5,28 @@ nombre_un = input("inserer le premier nombre : \n")
 nombre_deux = input("inserer le second nombre : \n")
 if( nombre_un.isdigit() & nombre_deux.isdigit() ):
     print(f"la somme de {nombre_un} et {nombre_deux} = {int(nombre_un) + int(nombre_deux)}")
-else:
+elif(nombre_un.isalnum() and nombre_deux.isalnum()):
     print("impossible de faire l'addition !")
+    nombre_un = input("inserer le premier nombre : \n")
+    nombre_deux = input("inserer le second nombre : \n")
+    if( nombre_un.isdigit() & nombre_deux.isdigit() ):
+        print(f"la somme de {nombre_un} et {nombre_deux} = {int(nombre_un) + int(nombre_deux)}")
+    else:
+        print("pas d'addition avec des alphanum désolé")
+    
+# On déclare deux variables
+a = b = ""
+
+# Tant que a et b ne sont pas des nombres, on boucle
+while not (a.isdigit() and b.isdigit()):
+    
+    # On demande deux nombres à l'utilisateur
+    a = input("Entrez un premier nombre : ")
+    b = input("Entrez un deuxième nombre : ")
+    
+    # On affiche une phrase si les nombres entrés ne sont pas valides.
+    if not (a.isdigit() and b.isdigit()):
+        print("Veuillez entrer deux nombres valides")
+
+# On affiche le résultat de l'addition
+print(f"Le résultat de l'addition de {a} avec {b} est égal à {int(a) + int(b)}")
