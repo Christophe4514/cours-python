03d4f891545df65e35a8385a3d631baf6b81fbbd
christophe4514 <christophewizamo@gmail.com>
2023-05-26 15:14:08 +0100

    exament 1ere partie

---
+++ cours/premiere_partie/exament.py
@@ -0,0 +1,63 @@
+# a = 5
+# print("La valeur contenue dans la variable a est " + int(a))
+_bt = 0
+# print(_bt)
+# print(type(a))
+liste = list()
+# print(liste
+# a=b=5=c
+# print(True+1)
+# print(len(5))
+# age = 12
+# if age < 12:
+#     print("Vous avez moins de 12 ans.")
+# if age < 30:
+#     print("Vous avez moins de 30 ans.")
+# if age < 60:
+#     print("Vous avez moins de 60 ans.")
+# print(5 < 10 and (10 > 5 or 5 > 2))
+# print(not True and 10 == 10)
+# note = 5
+# if note < 10:
+#     print("Vous n'avez pas la moyenne.")
+# if note >= 10 and note < 14:
+#     print("Peut mieux faire...")
+# if note >= 14 and note < 19:
+#     print("Bravo !")
+# else:
+#     print("Vous êtes le meilleur !")
+# print(50 + "50")
+# print(list(range(10)))
+# i = 1
+# while i < 10:
+#     print(f"Itération {i}")
+#     i += 1
+
+# liste = []
+# while liste is True:
+#     print("Itération sur la liste")
+
+# a = 5
+# str(a)
+# print(type(a))
+
+# a = 5
+#     print(a)
+
+# user = "admin"
+# mdp = "admin"
+# if mdp == "admin" or user == "admin":
+#     print("Bienvenue !")
+
+
+
+# i = 0
+# while True:
+#     print(i)
+#     i += 1
+#     if i == 10:
+#         continue
+
+# a = 5
+# b = "10"
+# print(f"Le résultat de l'addition de {a} avec {b} est égal à {a + b}")
