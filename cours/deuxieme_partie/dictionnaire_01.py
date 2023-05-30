f6a6332e2968bbef6c99c8babd7eba4153999ccd
christophe4514 <christophewizamo@gmail.com>
2023-05-30 11:43:11 +0100

    dictionnaire suite

--- cours/deuxieme_partie/dictionnaire_01.py
+++ cours/deuxieme_partie/dictionnaire_01.py
@@ -1,4 +1,6 @@
 # DICTIONAIRE
+# https://www.docstring.fr/glossaire/dictionnaire/
+
 dictionnaire = {"nom": "christophe",
                 "proffession": "Ingénieur",
                 "age": 21,
@@ -62,3 +64,198 @@ print(dic.get(3, "not found"))
 if 3 in dic:
     del dic[3]
     print("delete successfully !")
+
+# 
+# bouclé sur un dic
+# keys et values deux possibilités de bouclé sur les dictionnaires
+# for cle in dictionnaire:
+    # print(cle)
+    # print(dictionnaire[cle]) //retourne les value assosié à la cle
+
+# on peut aussi utiliser la methode dictionnaire.items(), ceci retourne un tuple de cle et valeur
+# for cle, valeur in dictionaire.items():
+    # print(cle, valeur)
+
+# exemple
+for cle, valeur in dic.items():
+    print(cle, valeur)
+    # print(dic.items())
+print(dic.keys())
+print(dic.values())
+
+employes = {
+            "id01": {"prenom": "Paul", "nom": "Dupont", "age": 32},
+            "id02": {"prenom": "Julie", "nom": "Dupuit", "age": 25},
+            "id03": {"prenom": "Patrick", "nom": "Ferrand", "age": 36}
+            }
+            
+del employes["id03"]
+employes["id02"]["age"] = 26
+age_paul = employes["id01"]["age"]
+
+# recap
+"""
+En théorie
+d = {
+    clé: valeur,
+    clé: valeur,
+    clé: valeur,
+    ...
+    clé: valeur
+}
+"""
+# Dictionnaire vide
+d = {}
+print(d)
+# Dictionnaire dont les clés ne sont que des chaînes de caractères
+d = {
+    'spam': 'eggs',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+print(d)
+# Dictionnaire dont les clés sont des objets de différents types
+d = {
+    1: 'one',
+    'deux': 2,
+    (3, 4, 5): 'pas_de_soucis',
+    9.9: 'nine_point_nine'
+}
+print(d)
+"""Les dictionnaires que je viens de créer n'ont aucun sens, je voulais juste te montrer ce qu'il était possible de faire.
+Vous pouvez aussi créer un dictionnaire grâce la fonction dict:"""
+d = dict()  # {}
+d = dict({
+    'spam': 'eggs',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+})
+print(d)
+"""Une clé doit être unique, les doublons ne sont pas autorisés.
+Si cela arrive, cela ne va pas créer d'erreur mais votre seconde clé écrasera la première."""
+d = {
+    'spam': 'eggs',
+    'knights': 'lumberjack',  # 1ère clé 'knights'
+    'bacon': 'sausage',
+    'knights': 'ham'  # 2ème clé 'knights' qui va écraser la première
+}
+print(d)
+# Une clé ne peut pas être un objet muable :
+d = {
+    ['spam']: 'eggs'  # TypeError: unhashable type: 'list'
+}
+
+d = {
+    'spam': 'eggs',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'    
+}
+d['spam']  # 'eggs'
+# Si on essaie d'accéder à une clé inexistante avec les crochets, on a une erreur (KeyError)
+# print(d['ham'])
+d.get('spam')  # 'eggs'
+# Avec get, si la clé n'existe pas, on récupère None
+d.get('ham')  # None
+# Ou la valeur par défaut que l'on passe en deuxième argument
+d.get('ham', "Cette clé n'existe pas...")  # "Cette clé n'existe pas..."
+
+d = {
+    'spam': 'eggs',
+    'knights': 'lumberjack',   
+}
+d['spam'] = 'ham' # Clé existe déjà, remplace la valeur
+d['bacon'] = 'sausage' # Nouvelle clé, on créer la paire clé/valeur dans le dico
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+item = d.pop('knights') # 'lumberjack'
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+item = d.popitem() # ('bacon', 'sausage') OU ('spam', 'ham') OU ('knights': 'lumberjack')
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+d.clear() # {}
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+del d
+print(d) # NameError: name 'd' is not defined
+
+
+from collections import defaultdict
+programming_languages = defaultdict(lambda: 'Python')
+programming_languages['.js'] = 'JavaScript'
+programming_languages['.php'] = 'PHP'
+print(programming_languages['.js'])   # 'Javascript'
+print(programming_languages['.php'])  # 'PHP'
+print(programming_languages['.py'])   # 'Python' => Valeur par défaut
+
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+for key in d:
+    print(key)
+
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+for key in d:
+    print(key, d[key])
+
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+for key in d.keys():
+    print(key)
+keys = d.keys()
+print(keys)
+
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+for value in d.values():
+    print(value)
+
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+for key, value in d.items():
+    print(key, value)
+
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+print('bacon' in d)  # True
+print('ham' in d)    # False
